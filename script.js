624a57ae73c4950cae76cb4871b6ea815e196e3d
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-29 20:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
 //your JS code here. If required.
+
+let output=document.getElementById('output');
+
+function getNumber(){
+	return new Promise(res=>{
+		setTimeout(()=>{
+			let number=Number(document.getElementById('ip').value);
+			output.textContent=`Result:${number}`;
+			res(number);
+		},2000);
+	})
+}
+
+function mulNumber(num){
+	return new Promise(res=>{
+		setTimeout(()=>{
+			let ans=num*2;
+			output.textContent=`Result:${ans}`;
+			res(ans);
+		},1000);
+	})
+}
+function subNumber(num){
+	return new Promise(res=>{
+		setTimeout(()=>{
+			let ans=num-3;
+			output.textContent=`Result:${ans}`;
+			res(ans);
+		},1000);
+	})
+}
+function divNumber(num){
+	return new Promise(res=>{
+		setTimeout(()=>{
+			let ans=(num/2);
+			output.textContent=`Result:${ans}`;
+			res(ans);
+		},1000);
+	})
+}
+function addNumber(num){
+	return new Promise(res=>{
+		setTimeout(()=>{
+			let ans=num+10;
+			output.textContent=`Result:${ans}`;
+			res(ans);
+		},1000);
+	})
+}
+
+function finNumber(num){
+	return new Promise(res=>{
+			output.textContent=`Final Result:${num}`;
+		res(ans);
+	})
+}
+function errorMsg(err){
+			output.textContent=`Error: ${error.message}`;
+}
+document.getElementById('btn').addEventListener('click', () => {
+getNumber()
+.then(mulNumber)
+.then(subNumber)
+.then(addNumber)
+.then(divNumber)
+.then(finNumber)
+.catch(errorMsg)
+})
