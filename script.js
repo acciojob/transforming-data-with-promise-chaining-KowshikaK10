f40861ca58a551cdc0265fdeeef3d3460b28e30f
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-29 20:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function addNumber(num){
 	return new Promise(res=>{
 		setTimeout(()=>{
 			let ans=num+10;
-			output.textContent=`Final Result:${num}`;
+			output.textContent=`Result:${ans}`;
 			res(ans);
 		},1000);
 	})
@@ -58,5 +58,8 @@ getNumber()
 .then(subNumber)
 .then(divNumber)	
 .then(addNumber)
+.then(res=>{
+	output.textContent=`Final Result:${res}`;
+})
 .catch(errorMsg)
 })
